ae09259d2807315381e2453c390c6239db26fa32
milicadukic15 <milica15dukic@gmail.com>
2026-02-05 18:02:14 +0100

    hotel.cs

--- Server/Hotel.cs
+++ Server/Hotel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Domen.Entiteti;
+using Domen.Enumeracije;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,41 @@ namespace Server
 {
     public class Hotel
     {
+        public List<Apartman> Apartmani { get; set; } = new List<Apartman>();
+
+        public Hotel()
+        {
+            Apartmani.Add(new Apartman { BrojApartmana = 101, Klasa = KlasaApartmana.PRVA, MaxBrojGostiju = 2, Stanje = StanjeApartmana.PRAZAN });
+            Apartmani.Add(new Apartman { BrojApartmana = 202, Klasa = KlasaApartmana.DRUGA, MaxBrojGostiju = 4, Stanje = StanjeApartmana.POTREBNO_CISCENJE });
+            Apartmani.Add(new Apartman { BrojApartmana = 303, Klasa = KlasaApartmana.TRECA, MaxBrojGostiju = 1, Stanje = StanjeApartmana.ZAUZET });
+        }
+
+        public string ObradiRezervaciju(KlasaApartmana klasa, int brojGostiju)
+        {
+            Apartman ap = Apartmani.FirstOrDefault(a => a.Stanje == StanjeApartmana.PRAZAN
+                                              && a.Klasa == klasa
+                                              && a.MaxBrojGostiju >= brojGostiju);
+            if (ap != null)
+            {
+                ap.Stanje = StanjeApartmana.ZAUZET;
+                return $"POTVRDA - Rezervisana soba {ap.BrojApartmana}";
+            }
+            return "ODBIJENO - Nema slobodnih soba tražene klase.";
+        }
+
+        public void AktivirajAlarm(int brojSobe)
+        {
+            Apartman ap = Apartmani.FirstOrDefault(a => a.BrojApartmana == brojSobe);
+            if (ap != null) ap.Alarm = StanjeAlarma.AKTIVIRAN;
+        }
+
+        public void RegistrujNarudzbinu(int brojSobe, string stavka)
+        {
+            Apartman ap = Apartmani.FirstOrDefault(a => a.BrojApartmana == brojSobe);
+            if (ap != null)
+            {
+                Console.WriteLine($"Narudžbina '{stavka}' zabeležena za sobu {brojSobe}.");
+            }
+        }
     }
 }
